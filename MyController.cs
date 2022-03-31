eef83e858e9d485e68c66d2ec7082f7af0066dfc
Alphons van der Heijden <alphons@heijden.com>
2022-03-31 14:32:33 +0200

    Update on the bug

--- MyController.cs
+++ MyController.cs
@@ -1,20 +1,24 @@
 ﻿
 using Microsoft.AspNetCore.Mvc;
 
+#nullable enable
+
 namespace TestWeb
 {
 	public class MyController : ControllerBase
 	{
 		[HttpGet]
 		[Route("~/")]
-		public async Task<IActionResult> Index(List<string> list)
+		public async Task<IActionResult> Index(List<string?> list)
 		{
 			await Task.Yield();
 
 			// Returned list "a", b", "b",  "c"
 			// Expected list "a", b", null, "c"
 
-			return Content("<pre>returns "+string.Join(",", list)+ " expected a,b,,c</pre>", "text/html");
+			var expected = new List<string?>() { "a", "b", null, "c" };
+
+			return Content($"<pre>returns {string.Join(",", list)} expected {string.Join(",", expected)}</pre>", "text/html");
 
 		}
 	}
--- Program.cs
+++ Program.cs
@@ -1,19 +1,58 @@
 
+using System.Diagnostics;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var ModelBinderProviders = new string[] {
+		"BinderTypeModelBinderProvider",
+		"ServicesModelBinderProvider",
+		"BodyModelBinderProvider",
+		"HeaderModelBinderProvider",
+		"FloatingPointTypeModelBinderProvider",
+		"EnumTypeModelBinderProvider",
+		"DateTimeModelBinderProvider",
+		"CancellationTokenModelBinderProvider",
+		"ByteArrayModelBinderProvider",
+		"FormFileModelBinderProvider",
+		"FormCollectionModelBinderProvider",
+		"KeyValuePairModelBinderProvider",
+		"DictionaryModelBinderProvider",
+		"ComplexObjectModelBinderProvider",
+		"ArrayModelBinderProvider",
+
+		// "CollectionModelBinderProvider", // Dont remove for this test
+		// "SimpleTypeModelBinderProvider", // Dont remove for this test
+	};
 
 builder.Services.AddMvcCore().AddMvcOptions(options =>
 {
-	options.ModelMetadataDetailsProviders.Add(new TestWeb.NoNullMetadataProvider()); // no effect
-	options.ValueProviderFactories.Insert(0, new TestWeb.SomeValueProviderFactory());
+	options.OutputFormatters.Clear();
+	options.InputFormatters.Clear();
+	options.ValueProviderFactories.Clear();
+	options.ModelValidatorProviders.Clear();
+	options.Conventions.Clear();
+	options.Filters.Clear();
+	options.ModelMetadataDetailsProviders.Clear();
+	options.ModelValidatorProviders.Clear();
+
+	// Removing binderproviders not used in this test
+	foreach (var binder in options.ModelBinderProviders.ToArray())
+	{
+		var name = binder.GetType().Name;
+		if (ModelBinderProviders.Contains(name))
+		{
+			Debug.WriteLine($"Removing \"{name}\"");
+			options.ModelBinderProviders.Remove(binder);
+		}
+	}
+
+	options.ValueProviderFactories.Add(new TestWeb.SomeValueProviderFactory());
 });
 
 var app = builder.Build();
 
 app.UseRouting();
 
-app.UseEndpoints(endpoints =>
-{
-    endpoints.MapControllers();
-});
+app.MapControllers();
 
 app.Run();
